89ac5e0dbfb8f8b5bc2bf53c36dc20c404cbf41b
frankljin <52306663+frankljin@users.noreply.github.com>
2021-02-13 16:17:41 -0500

    Navarbar and stuff

--- client/src/Components/LandingPage/landing.js
+++ client/src/Components/LandingPage/landing.js
@@ -1,13 +1,25 @@
 import React, {useState, useEffect} from 'react'
+import NavBar from '../Partials/navbar'
+import './landing.css'
+import {Card, Button} from 'react-bootstrap'
+
 
 const LandingPage = () => {
 
-
-
     return (
-        <div>Landing Component</div>
-    )
-
+        <div id="main">
+            <NavBar/>
+            <div className="container" id="homecont">
+                <Card className="text-center">
+                    <Card.Body id="homecard">
+                        <Card.Title><h1>DaBabyThon</h1></Card.Title>
+                        <Card.Text>I don't know what to write here.</Card.Text>
+                        <Button variant="primary">Go to Dhruv's Form</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+  );
 }
 
 export default LandingPage
