17baf70d2fc46731bf6e68b87959fb509c8660b2
Eric Bian <bianyaoda06@gmail.com>
2021-02-13 14:17:41 -0500

    basic routes complete

--- server/app.js
+++ server/app.js
@@ -5,6 +5,25 @@ const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
 const passport=require("passport");
 const axios=require("axios");
+
+
 //
 const app=express();
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017//dbname",{useNewUrlParser:true,useUnifiedTopology:true});
+
+
+
+app.post("/",(req,res)=>{
+
+})
+
+
+
+
+
+
+app.listen(process.env.PORT||3000,()=>{
+    console.log("app currently running on port 3000");
+})
